b741a6d1e42fdbda98cda247097037e14cec032c
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-10-23 15:40:04 +0000

    Generated comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :photos
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  root "photos#index"
 end
