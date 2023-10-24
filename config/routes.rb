cf9e0107cfa53ce0c653a7c20f45a889f6f1df6b
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-10-24 20:58:24 +0000

    set up show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "photos#index"
-
+  
   devise_for :users
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-
+  resources :users, only: :show
+  
 end
